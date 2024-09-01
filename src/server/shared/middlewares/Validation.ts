ddf450ae789743a32dc91eb3e23ff484170e6461
William <william.gispiela@gmail.com>
2024-09-01 12:36:43 -0300

    fix: devido atualização do yup foi necessário mudar de Schema/SchemaOf para ObjectSchema em Validation.ts

--- src/server/shared/middlewares/Validation.ts
+++ src/server/shared/middlewares/Validation.ts
@@ -1,15 +1,15 @@
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { Schema, ValidationError } from 'yup';
+import { AnyObject, Maybe, ObjectSchema, ValidationError } from 'yup';
 
 
 
 type TProperty = 'body' | 'header' | 'params' | 'query';
 
-type TGetSchema = <T>(schema: Schema<T>) => Schema<T>
+type TGetSchema = <T extends Maybe<AnyObject>>(schema: ObjectSchema<T>) => ObjectSchema<T>
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type TAllSchemas = Record<TProperty, Schema<any>>;
+type TAllSchemas = Record<TProperty, ObjectSchema<any>>;
 
 type TGetAllSchemas = (getSchema: TGetSchema) => Partial<TAllSchemas>;
 
